112b10622220a27a75a9924eeadf320b49876cf5
João L. S. Azevedo <55589171+JoaoLucas976@users.noreply.github.com>
2023-10-09 10:26:48 -0300

    Ajustando Formatação

--- codigo_de_teste/codigo_dos_testes.py
+++ codigo_de_teste/codigo_dos_testes.py
@@ -1,5 +1,4 @@
 import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import tkinter as tk
@@ -9,14 +8,16 @@ import pandas as pd
 from datetime import datetime
 import serial as sr  # Rename the 'serial' module to 'sr'
 import serial.tools.list_ports
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 # ------global variables
 arrayDadosTermopar = np.array([0])
 arrayDadosPirometro = np.array([0])
-cols = ['Termopar', 'Pirômetro','Data/Hora']
+cols = ['Termopar', 'Pirômetro', 'Data/Hora']
 dat = pd.DataFrame(columns=cols)
 sensores = 0
 cond = False
-#Vaiável de data para armazenar o tempo 
+# Vaiável de data para armazenar o tempo
 now = datetime.now()
 data = now.strftime("%d/%m/%Y %H:%M:%S")
 
@@ -26,9 +27,9 @@ fig = Figure(figsize=(8, 6))  # Adjust the figure size to fit the entire GUI win
 
 ax1 = fig.add_subplot(211)
 
-ax1.set_title('Termopar (MAX6675)',fontsize=18)
-ax1.set_xlabel('Tempo',fontsize=18)
-ax1.set_ylabel('Temperadura °C',fontsize=18)
+ax1.set_title('Termopar (MAX6675)', fontsize=18)
+ax1.set_xlabel('Tempo', fontsize=18)
+ax1.set_ylabel('Temperadura °C', fontsize=18)
 ax1.set_xlim(0, 40)
 ax1.grid()
 
@@ -36,21 +37,20 @@ fig.subplots_adjust(hspace=0.5)
 
 ax2 = fig.add_subplot(212)
 
-ax2.set_title('Pirômetro (MLX90614)',fontsize=18)
-ax2.set_xlabel('Tempo',fontsize=18)
-ax2.set_ylabel('Temperadura °C',fontsize=18)
+ax2.set_title('Pirômetro (MLX90614)', fontsize=18)
+ax2.set_xlabel('Tempo', fontsize=18)
+ax2.set_ylabel('Temperadura °C', fontsize=18)
 ax2.set_xlim(0, 40)
 ax2.grid()
 
 lines1 = ax1.plot([], [])[0]  # variaveis que recebe os valores de x e de y
-lines2 = ax2.plot([], [])[0]
 lines2 = ax2.plot([], [], color="r")[0]
 
 
 def primeiroGrafico(dadoTermopar):
     global arrayDadosTermopar
 
-    if (len(arrayDadosTermopar) < 40):
+    if len(arrayDadosTermopar) < 40:
         arrayDadosTermopar = np.append(arrayDadosTermopar, float(dadoTermopar))
         limiteMaximo = round(np.amax(arrayDadosTermopar, axis=0))
         limiteMinimo = round(np.amin(arrayDadosTermopar, axis=0))
@@ -60,7 +60,6 @@ def primeiroGrafico(dadoTermopar):
         limiteMaximo = round(np.amax(arrayDadosTermopar, axis=0))
         limiteMinimo = round(np.amin(arrayDadosTermopar, axis=0))
         ax1.set_ylim(limiteMinimo-20, limiteMaximo+20)
-        
 
         arrayDadosTermopar[0:39] = arrayDadosTermopar[1:40]
         arrayDadosTermopar[39] = float(dadoTermopar)
@@ -71,7 +70,7 @@ def primeiroGrafico(dadoTermopar):
 
 def segundoGrafico(dadoPirometro):
     global arrayDadosPirometro
-    if (len(arrayDadosPirometro) < 40):
+    if len(arrayDadosPirometro) < 40:
         arrayDadosPirometro = np.append(
             arrayDadosPirometro, float(dadoPirometro))
 
@@ -86,7 +85,7 @@ def segundoGrafico(dadoPirometro):
         ax2.set_ylim(limiteMinimo-20, limiteMaximo+20)
 
         arrayDadosPirometro[0:39] = arrayDadosPirometro[1:40]
-        arrayDadosPirometro[39] =float(dadoPirometro)
+        arrayDadosPirometro[39] = float(dadoPirometro)
     lines2.set_xdata(np.arange(0, len(arrayDadosPirometro)))
     lines2.set_ydata(arrayDadosPirometro)
 
@@ -94,7 +93,7 @@ def segundoGrafico(dadoPirometro):
 def plot_data():
     global cond, data, dat
 
-    if (cond == True):
+    if cond:
         # -----------Variáveis dos que recebem os dados dos sensores---------
         primeiroDado = sensores.readline()
         segundoDado = sensores.readline()
@@ -102,16 +101,15 @@ def plot_data():
         dadoTermopar = primeiroDado.decode('utf')
         dadoPirometro = segundoDado.decode('utf')
     
-        #print('termopar: ', (dadoTermopar))
-        #print('pirometro: ', (dadoPirometro))
+        # print('termopar: ', (dadoTermopar))
+        # print('pirometro: ', (dadoPirometro))
 
-        
-        #print("%.2f"%correcao)
+        # print("%.2f"%correcao)
         primeiroGrafico(dadoPirometro)
         segundoGrafico(dadoTermopar)
 
 # ----------Acessa a função pandas para plotar as strings com os dados dos sensores--------
-        dat = dat.append({'Termopar': dadoTermopar[0:5], 'Pirômetro': dadoPirometro[0:5],'Data/Hora': data}, ignore_index=True)
+        dat = dat.append({'Termopar': dadoTermopar[0:5], 'Pirômetro': dadoPirometro[0:5], 'Data/Hora': data}, ignore_index=True)
 
         # print(dat)
 
@@ -120,7 +118,7 @@ def plot_data():
     root.after(200, plot_data)
 
 # -----------Salvar os dados em arquivo csv----------
-    #dat.to_csv("termopar",index = True, sep =" ")
+    # dat.to_csv("termopar",index = True, sep =" ")
 
 
 # -----------Criação das funções------
@@ -163,26 +161,27 @@ def update_status():
     # After 1 second, update the status
     root.after(200, update_status)
 
-  
 
 # -----Main GUI code-----
 root = tk.Tk()
 root.title('Plotagem em tempo real')
 
-pid="0043"
+pid = "0043"
 com_port = None
 
+
 def connect():
-    global sensores, pid
+    global sensores, pid, com_port
     ports = list(sr.tools.list_ports.comports())
 
     for p in ports:
         if pid in p.hwid:
             com_port = p.device
-    print("porta atual: ",com_port)
+    print("porta atual: ", com_port)
     sensores = sr.Serial(com_port, baudrate=9600, timeout=1)
     porta = tk.Label(root, text=f"Conectado a porta: {com_port}", font=("Arial", 20))
     porta.pack()
+
 
 connect()
 
@@ -203,9 +202,12 @@ salvar_dados = tk.Button(root, text="Salvar Dados", font=(
 salvar_dados.pack(side=tk.LEFT)
 
 # Função para salvar a imagem
+
+
 def save_image():
     file_path = filedialog.asksaveasfilename(defaultextension='.png')
     canvas.print_figure(file_path, dpi=1200)  # Set the desired dpi (resolution)
+
 
 salvar_imagem = tk.Button(root, text="Salvar Imagem", font=(
     'calbiri', 12), command=save_image)
@@ -223,9 +225,11 @@ statusPirometro.pack(side=tk.LEFT)
 statusPirometro = tk.Label(root, text="Esperando dados", font=("Arial", 20))
 statusPirometro.pack(side=tk.LEFT)
 
+
 def salvar():
     file_path = filedialog.asksaveasfilename(defaultextension='.csv')
     dat.to_csv(file_path, index=False, sep=" ")
+
 
 root.after(1, update_status)
 root.after(1, plot_data)
