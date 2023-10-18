60f671124afb398e72b0087a9755d8824c2ac357
João L. S. Azevedo <55589171+JoaoLucas976@users.noreply.github.com>
2023-10-18 12:09:39 -0300

    Comentando funções

--- codigo_python/functions.py
+++ codigo_python/functions.py
@@ -3,6 +3,10 @@ from serial.tools import list_ports
 from time import sleep
 
 def check_ports():
+    """
+    Função para checar se existem portas conectadas
+    :return: Retorna a lista das portas conectadas
+    """
     ports = list(list_ports.comports())
     if len(ports) != 0:
         ports_list = [port.device for port in ports]
@@ -12,6 +16,12 @@ def check_ports():
 
 
 def open_popup(msg, width, root):
+    """
+    Função para mostrar um popup na tela
+    :param msg: Texto apresentado no popup
+    :param width: Tamanho usado para dimensionar a tela
+    :param root: Janela onde vai ser apresentada
+    """
     top = tk.Toplevel(root)
     top.geometry(f"{width}x100")
     top.title("Erro de porta")
